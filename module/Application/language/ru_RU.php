7bbcfb441bbdaf0fd88476abb9fb74556bda31d7
Narek <=narek.p@live.ru>
2018-03-15 12:16:02 +0300

    trans

--- module/Application/language/ru_RU.php
+++ module/Application/language/ru_RU.php
@@ -1,4 +1,18 @@
 <?php
 
 return [
+    'Password recovery' => 'Восстановление пароля',
+    'Submit' => 'Отправить',
+    'Submit your recovery e-mail address below.' => 'Введите адрес электронной почты ниже.',
+    'We will send you a secure link to reset your password shortly.' => 'Мы отправим вам безопасную ссылку, чтобы сбросить пароль в ближайшее время.',
+    'An email has been sent to %s.' => 'На адрес %s отправлено письмо.',
+    'It includes information on changing and confirming your new password.' => 'Оно содержит информацию об изменении и подтверждении вашего нового пароля.',
+    'Please reset your password within the next <b>1 hour</b>.' => 'Сбросьте пароль в течение следующего 1 часа.',
+    'If none of the above helps, please contact our support team' => 'Если ни одно из вышеперечисленных действий не поможет, обратитесь в нашу службу поддержки',
+    'Reset password' => 'Сброс пароля',
+    'Reset password for %s.' => 'Сброс пароля для %s.',
+    'Password' => 'Пароль',
+    'Password Verify' => 'Повтор пароля',
+    'Well done!' => 'Отлично!',
+    'Your password has been changed successfully!' => 'Ваш пароль был успешно изменен!',
 ];
